218d3102071f57e0d8d3967b1e698611b2f52710
Dency-Pambhar <dencypambhar@gmail.com>
2024-06-08 01:42:46 +0530

    Adding card compnenent, changes in the UI for side nav and virtual pitch

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 const Logo: React.FC = () => {
   return (
     <div>
-      <div className="flex items-center px-[1rem]">
+      <div className="flex items-center px-[1rem] mt-4">
         <Image
           src={"/Project_Logo.svg"}
-          width={50}
-          height={100}
+          width={30}
+          height={30}
           alt="Project Logo"
         />
-        <p className="text-[#dbdbdb] text-[24px] font-bold mx-[0.5rem]">
+        <p className="text-[#dbdbdb] text-[18px] font-bold mx-[0.5rem]">
           Discuz
         </p>
       </div>
