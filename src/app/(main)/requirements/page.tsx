a7ac5a9bb0cdfaf962dbf160d94dd3941accad2b
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-06-15 17:17:48 +0530

    API integration of requirement added

--- src/app/(main)/requirements/page.tsx
+++ src/app/(main)/requirements/page.tsx
@@ -1,8 +1,11 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { CardWithForm } from "../page-components/Card";
 import SideNav from "../page-components/Side-Nav";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const Requirements = () => {
   const contents = [
@@ -39,6 +42,25 @@ const Requirements = () => {
       date: "12 March 2024",
     },
   ];
+
+  let userData: any = sessionStorage.getItem("userData");
+  userData = JSON.parse(userData);
+
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ["requirements-requests"],
+    queryFn: async () => {
+      const response = await axios.post(
+        `http://localhost:3000/requirements/email`,
+        {
+          email: `${userData?.user?.email}`,
+        }
+      );
+      return response.data;
+    },
+  });
+
+  console.log(data);
+
   return (
     <>
       <div className="flex justify-around">
@@ -67,13 +89,13 @@ const Requirements = () => {
                 {/* {data?.data?.response?.requirements.map((content:any) => {
                   <CardWithForm image={content.imgPath} isRequested={!content.isAccepted} title={content.title} description={content.description} />
                 })} */}
-                {contents.map((content: any) => {
+                {data?.data?.map((content: any) => {
                   return (
                     <CardWithForm
                       image={content.imgPath}
                       isRequested={!content.isAccepted}
-                      title={content.title}
-                      description={content.description}
+                      title={content.product}
+                      description={content.product_desc}
                     />
                   );
                 })}
