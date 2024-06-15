15b58eeac5ccfa1957a8a42b6817342eb727de4f
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-06-15 17:57:49 +0530

    Loading animation added in the my requirement page

--- src/app/(main)/requirements/page.tsx
+++ src/app/(main)/requirements/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import "../../../../public/style/scroll-bar.css";
+import "../../../../public/style/spinner.css";
 
 const Requirements = () => {
   const contents = [
@@ -60,53 +61,64 @@ const Requirements = () => {
     },
   });
 
-  console.log(data);
-
-  return (
-    <>
-      <div className="flex justify-around">
-        <SideNav />
-        <div className="w-[78%] h-[98vh] my-[1vh]">
-          <div className="rounded-3xl pb-4 bg-[#fff] ">
-            <div className="flex items-center w-[92%] justify-between mx-auto pt-4 mb-2">
-              <h2 className="text-[#3E435D]  text-[28px] font-semibold">
-                Hello Dency!
-              </h2>
-              <div className="flex w-[35%]">
-                <Input
-                  className="rounded-xl border-gray-300 placeholder:text-gray-400 border-2"
-                  type="text"
-                  placeholder="Search..."
-                />
-                <Link href={"/post_requirement"}>
-                  <Button className="cursor-pointer hover:bg-blue-500 bg-blue-500 text-[#fff] rounded-2xl ml-4">
-                    Post Requirement
-                  </Button>
-                </Link>
+  if (isPending) {
+    return (
+      <>
+        <div className="flex justify-around">
+          <SideNav />
+          <div className="flex w-[78%] items-center justify-center">
+            <span className="loader"></span>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="flex justify-around">
+          <SideNav />
+          <div className="w-[78%] h-[98vh] my-[1vh]">
+            <div className="rounded-3xl pb-4 bg-[#fff] ">
+              <div className="flex items-center w-[92%] justify-between mx-auto pt-4 mb-2">
+                <h2 className="text-[#3E435D]  text-[28px] font-semibold">
+                  Hello Dency!
+                </h2>
+                <div className="flex w-[35%]">
+                  <Input
+                    className="rounded-xl border-gray-300 placeholder:text-gray-400 border-2"
+                    type="text"
+                    placeholder="Search..."
+                  />
+                  <Link href={"/post_requirement"}>
+                    <Button className="cursor-pointer hover:bg-blue-500 bg-blue-500 text-[#fff] rounded-2xl ml-4">
+                      Post Requirement
+                    </Button>
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-row overflow-y-scroll no-scrollbar h-[87vh] mx-auto w-[92%]">
-              <div className="flex flex-row  justify-between flex-wrap">
-                {/* {data?.data?.response?.requirements.map((content:any) => {
-                  <CardWithForm image={content.imgPath} isRequested={!content.isAccepted} title={content.title} description={content.description} />
-                })} */}
-                {data?.data?.map((content: any) => {
-                  return (
-                    <CardWithForm
-                      image={content.imgPath}
-                      isRequested={!content.isAccepted}
-                      title={content.product}
-                      description={content.product_desc}
-                    />
-                  );
-                })}
+              <div className="flex flex-row overflow-y-scroll no-scrollbar h-[87vh] mx-auto w-[92%]">
+                <div className="flex flex-row  justify-between flex-wrap">
+                  {/* {data?.data?.response?.requirements.map((content:any) => {
+                    <CardWithForm image={content.imgPath} isRequested={!content.isAccepted} title={content.title} description={content.description} />
+                  })} */}
+                  {data?.data?.map((content: any) => {
+                    return (
+                      <CardWithForm
+                        image={content.imgPath}
+                        isRequested={false}
+                        title={content.product}
+                        description={content.product_desc}
+                      />
+                    );
+                  })}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default Requirements;
