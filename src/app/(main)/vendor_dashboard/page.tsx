96fcdaf3ceda4591bd8bafa5c2c01939901d9310
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-06-15 23:52:12 +0530

    API integration in Vendor Dashboard Page

--- src/app/(main)/vendor_dashboard/page.tsx
+++ src/app/(main)/vendor_dashboard/page.tsx
@@ -14,61 +14,42 @@ import SideNav from "../page-components/Side-Nav";
 import "../../../../public/style/scroll-bar.css";
 
 const Dashboard = () => {
-  // const router = useRouter();
-  // if (!sessionStorage.getItem("userData")) {
-  //   router.push("/login");
-  // }
-
   let userData: any = sessionStorage.getItem("userData");
   userData = JSON.parse(userData);
 
+  console.log(userData?.user?.sector);
+
   const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ["requirements-requests"],
-    queryFn: () =>
-      axios.get(`http://3.6.132.27/api/requirements/${userData.user.email}`),
+    queryKey: ["requirements_for_vendor"],
+    queryFn: async () => {
+      const response = await axios.post(
+        `http://localhost:3000/requirements/sector`,
+        {
+          req_sector: "Automobile",
+        }
+      );
+      return response.data;
+    },
   });
 
-  const contents = [
-    {
-      imgPath: "/display-picture/display.png",
-      isAccepted: true,
-      title: "ABC Company",
-      description:
-        "Description about the company and the project it is offering ...",
-      date: "12 March 2024",
-    },
-    {
-      imgPath: "/display-picture/display.png",
-      isAccepted: true,
-      title: "ABC Company",
-      description:
-        "Description about the company and the project it is offering ...",
-      date: "12 March 2024",
-    },
-    {
-      imgPath: "/display-picture/display.png",
-      isAccepted: true,
-      title: "ABC Company",
-      description:
-        "Description about the company and the project it is offering ...",
-      date: "12 March 2024",
-    },
-    {
-      imgPath: "/display-picture/display.png",
-      isAccepted: true,
-      title: "ABC Company",
-      description:
-        "Description about the company and the project it is offering ...",
-      date: "12 March 2024",
-    },
-  ];
-  if (true) {
+  if (isPending) {
+    return (
+      <>
+        <div className="flex justify-around">
+          <SideNav />
+          <div className="flex w-[78%] items-center justify-center">
+            <span className="loader"></span>
+          </div>
+        </div>
+      </>
+    );
+  } else {
     return (
       <div className="flex justify-around">
         <SideNav />
         <div className="w-[78%] h-[98vh] my-[1vh]">
           <div className="rounded-3xl pb-4 bg-[#fff] ">
-            <div className="flex items-center w-[92%] justify-between mx-auto pt-4 mb-2">
+            <div className="flex items-center w-[97%] justify-between mx-auto pt-4 mb-2">
               <h2 className="text-[#3E435D]  text-[28px] font-semibold">
                 Hello Dency!
               </h2>
@@ -80,18 +61,17 @@ const Dashboard = () => {
                 />
               </div>
             </div>
-            <div className="flex flex-row overflow-y-scroll no-scrollbar h-[87vh] mx-auto w-[92%]">
+            <Separator className="bg-[#d7d7d7] w-[97%] mx-auto my-2  h-[2px]" />
+
+            <div className="flex flex-row overflow-y-scroll no-scrollbar h-[87vh] mx-auto w-[97%]">
               <div className="flex flex-row  justify-between flex-wrap">
-                {/* {data?.data?.response?.requirements.map((content:any) => {
-                  <CardWithForm image={content.imgPath} isRequested={!content.isAccepted} title={content.title} description={content.description} />
-                })} */}
-                {contents.map((content: any) => {
+                {data?.data?.map((content: any) => {
                   return (
                     <CardWithForm
-                      image={content.imgPath}
-                      isRequested={!content.isAccepted}
-                      title={content.title}
-                      description={content.description}
+                      image={content.imgPath || "/images/image.png"}
+                      isRequested={false}
+                      title={content.product}
+                      description={content.product_desc}
                     />
                   );
                 })}
