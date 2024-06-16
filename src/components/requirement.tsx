0ed9c5e61cf64a059e123325fd339ab6b8dcc9cc
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-06-16 15:13:49 +0530

    Redirection for back button in post reqruirement page changed

--- src/components/requirement.tsx
+++ src/components/requirement.tsx
@@ -166,7 +166,7 @@ const Requirement: React.FC = () => {
             </div>
           </form>
           <Button
-            onClick={() => router.push("/client_dashboard")}
+            onClick={() => router.push("/requirements")}
             className="w-[100%] bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
           >
             Back
