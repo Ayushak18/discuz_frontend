62cc27d98cb21059bd3791007720ad02dda1cc7d
Dency-Pambhar <dencypambhar@gmail.com>
2024-06-19 15:48:46 +0530

    Adding form validation to virtual pitch

--- src/components/pitch.tsx
+++ src/components/pitch.tsx
@@ -1,28 +1,41 @@
 "use client";
+
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-import { useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-  SelectGroup,
-} from "@/components/ui/select";
-import { useState } from "react";
 
-const Requirement: React.FC = () => {
-  const [descriptionError, setDescriptionError] = useState("");
-  const [isDescriptionValid, setIsDescriptionValid] = useState(true);
+const Pitch: React.FC = () => {
+  const router = useRouter();
+  const [pitchBrief, setPitchBrief] = useState("");
+  const [validationMessage, setValidationMessage] = useState("");
+
+  const { data } = useQuery({
+    queryKey: ["requirements-requests"],
+    queryFn: () =>
+      axios.get(`http://localhost:3000/requirements/6671e14dd6e73c6d3c43471f`),
+  });
+
+  // To Post the Pitch to a particular Requirement
+  const {
+    mutate,
+    isError,
+    isSuccess,
+  }: any = useMutation({
+    mutationFn: (pitch) => {
+      return axios
+        .post("http://localhost:3000/virtual_pitch/post", pitch)
+        .then((res) => res.data);
+    },
+  });
 
   const notify = () =>
-    toast.info("Requirement Created Successfully!", {
+    toast.info("Pitch Created Successfully!", {
       toastId: "success1",
       position: "bottom-right",
       autoClose: 3000,
@@ -34,7 +47,7 @@ const Requirement: React.FC = () => {
       theme: "colored",
     });
 
-  const error = () => {
+  const errorToast = () => {
     toast.error("Something went wrong!", {
       toastId: "error1",
       position: "bottom-right",
@@ -48,159 +61,161 @@ const Requirement: React.FC = () => {
     });
   };
 
-  let user: any = sessionStorage.getItem("userData");
-  user = JSON.parse(user);
-  console.log(user);
-  const { mutate, isError, isSuccess, isPending, data }: any = useMutation({
-    mutationFn: (requirement) => {
-      return axios
-        .post("http://localhost:3000/requirements/post", requirement)
-        .then((res) => res.data);
-    },
-  });
-
-  const validateDescription = (description: string) => {
-    const wordCount = description.trim().split(/\s+/).length;
-    if (wordCount < 100) {
-      setIsDescriptionValid(false);
-      setDescriptionError("Description must be at least 100 words.");
-      return false;
-    } else {
-      setIsDescriptionValid(true);
-      setDescriptionError("");
-      return true;
-    }
-  };
-
   if (isSuccess) {
     notify();
     setTimeout(() => {
-      router.push("/requirements");
+      router.push("/pitches");
     }, 2000);
   } else if (isError) {
-    error();
+    errorToast();
   }
 
-  const router = useRouter();
+  let user: any = sessionStorage.getItem("userData");
+  user = JSON.parse(user);
+
+  const validateInput = (input: string) => {
+    const wordCount = input.trim().split(/\s+/).length;
+    return wordCount >= 100;
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    if (validateInput(pitchBrief)) {
+      setValidationMessage("");
+      mutate({
+        company_name: user?.user?.name,
+        company_email: user?.user?.email,
+        place: event.target.location.value,
+        budget_min: event.target.price_min.value,
+        budget_max: event.target.price_max.value,
+        product_details: pitchBrief,
+        requirement_id: data?.data?._id,
+        pitch_title: event.target.pitch_title.value,
+      });
+    } else {
+      setValidationMessage("Input must be at least 100 words.");
+    }
+  };
+
   return (
     <>
-      {" "}
-      <div className="w-[80%] m-auto flex mb-4 flex-col h-full items-center">
-        <div className="text-center flex justify-center">
-          <h2 className="text-[#D9D9D9]  w-[60%]  mt-[2%] text-[20px] font-medium">
-            Post your requirement and get access to services across the world
+      <div className="w-[80%] m-auto flex flex-col h-full items-center">
+        <div className="text-center">
+          <h2 className="text-[#D9D9D9] my-[14%] text-[20px] font-medium">
+            <span className="text-[16px]">Create Your </span>
+            <span className="text-blue-500 font-bold text-[22px]">Vi</span>
+            rtual{" "}
+            <span className="text-blue-500 font-bold text-[22px]">Pi</span>tch
           </h2>
         </div>
 
+        <div className="w-[65%] text-justify p-2 px-4 border-2 border-gray-400 rounded-2xl text-[#fff]">
+          <p className="text-gray-300 text-start">
+            Requirement:{" "}
+            <span className="text-[14px] text-blue-500 font-medium">
+              {data?.data?.product}
+            </span>
+          </p>
+
+          <p className="line-clamp-2 text-gray-300 text-[12px] my-2">
+            {data?.data?.product_desc}
+          </p>
+        </div>
+
         {/* Form Section */}
-        <div className="w-[65%] mx-auto my-[3%]">
-          <form
-            onSubmit={(event: any) => {
-              event.preventDefault();
-              const description = event.target.description.value;
-
-              if (!validateDescription(description)) {
-                return;
-              }
-
-              mutate({
-                email: user.user.email,
-                product: event.target.product.value,
-                product_desc: description,
-                budget_min: event.target.min_budget.value,
-                budget_max: event.target.max_budget.value,
-                isAccepted: false,
-                req_sector: event.target.sector.value,
-                organisation_id: user?.user?.organisation_id,
-                organisation_email: user?.user?.email,
-              });
-            }}
-          >
-            <div className="flex flex-col mx-auto mt-[6%]">
-              <Label className="text-[#D9D9D9]" htmlFor="requirement">
-                Required service/product
-              </Label>
-              <textarea
-                required
-                name="product"
-                id="requirement"
-                className="bg-[#D9D9D9] mt-[10px] rounded w-full h-[60px] p-2 resize-none"
-              ></textarea>
-              <Label className="text-[#D9D9D9] mt-[5%]" htmlFor="text">
-                Description
-              </Label>
-              <textarea
-                required
-                name="description"
-                id="description"
-                className="bg-[#D9D9D9] mt-[10px]  rounded w-full p-2 resize-none"
-                rows={3}
-              ></textarea>
-              {!isDescriptionValid && (
-                <p className="text-red-500 mt-2">{descriptionError}</p>
-              )}
-            </div>
-            <div className="mt-[1.5rem]">
-              <Label className="text-[#D9D9D9] ">Sector</Label>
-              <Select required name="sector">
-                <SelectTrigger className="text-black bg-[#D9D9D9] rounded mb-[6%] w-full">
-                  <SelectValue placeholder="Select a sector" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup className="text-black bg-[#D9D9D9]">
-                    <SelectItem value="IT">IT</SelectItem>
-                    <SelectItem value="Human Resource">
-                      Human Resource
-                    </SelectItem>
-                    <SelectItem value="Manufacturing">Manufacturing</SelectItem>
-                    <SelectItem value="Automobile">Auto Mobiles</SelectItem>
-                    <SelectItem value="Furniture">Furniture</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="flex flex-col mt-[4%] w-[100%]">
-              <Label className="text-[#D9D9D9]">Budget</Label>
-              <div className="flex flex-row items-center">
-                <div className="mr-auto w-[40%] mt-2 flex flex-col">
-                  <Input
-                    required
-                    name="min_budget"
-                    placeholder=" Min."
-                    id="minPrice"
-                    className="bg-[#D9D9D9] mt-[5px] rounded"
-                    type="number"
-                  />
-                </div>
-                <div className="w-[40%] flex flex-col">
-                  <Input
-                    required
-                    name="max_budget"
-                    placeholder=" Max."
-                    id="maxPrice"
-                    className="bg-[#D9D9D9] mt-[5px] rounded"
-                    type="number"
-                  />
-                </div>
+        <form className="w-[100%]" onSubmit={handleSubmit}>
+          <div className="w-[65%] mx-auto mt-4 mb-4">
+            <div className="flex flex-row mt-[4%]">
+              <div className="w-full">
+                <Label className="text-[#D9D9D9] text-[12px]" htmlFor="title">
+                  Pitch Title
+                </Label>
+                <Input
+                  required
+                  name="pitch_title"
+                  id="title"
+                  className="bg-[#D9D9D9] mt-[5px] rounded"
+                  type="text"
+                />
               </div>
             </div>
-            <div className="flex flex-col mx-auto mt-[1rem]">
-              <Button className="my-[1rem] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <div className="w-full mt-4">
+              <Label className="text-[#D9D9D9] text-[12px]" htmlFor="location">
+                Location
+              </Label>
+              <Input
+                required
+                id="location"
+                className="bg-[#D9D9D9] mt-[5px] rounded"
+                type="text"
+              />
+            </div>
+            <div className="flex flex-row mt-[4%]">
+              <div className="mr-auto  w-[45%]">
+                <Label className="text-[#D9D9D9] text-[12px]" htmlFor="price">
+                  Price Min
+                </Label>
+                <Input
+                  required
+                  name="price_min"
+                  id="price"
+                  className="bg-[#D9D9D9] mt-[5px] rounded"
+                  type="number"
+                />
+              </div>
+              <div className="w-[45%]">
+                <Label
+                  className="text-[#D9D9D9] text-[12px]"
+                  htmlFor="location"
+                >
+                  Price Max
+                </Label>
+                <Input
+                  required
+                  name="price_max"
+                  className="bg-[#D9D9D9] mt-[5px] rounded"
+                  type="number"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col mx-auto mt-[6%]">
+              <Label className="text-[#D9D9D9] text-[12px]" htmlFor="pitch">
+                Brief Pitch
+              </Label>
+              <textarea
+                required
+                id="pitch"
+                name="pitch_brief"
+                className="bg-[#D9D9D9] mt-[10px] rounded w-full h-[72px] p-2 resize-none"
+                rows={3}
+                value={pitchBrief}
+                onChange={(e) => setPitchBrief(e.target.value)}
+              ></textarea>
+              {validationMessage && (
+                <p className="text-red-500 text-sm">{validationMessage}</p>
+              )}
+            </div>
+            <div className="flex flex-col mx-auto mt-[6%]">
+              <Button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
                 Submit
               </Button>
             </div>
-          </form>
-          <Button
-            onClick={() => router.push("/requirements")}
-            className="w-[100%] bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Back
-          </Button>
-        </div>
+          </div>
+        </form>
+        <Button
+          onClick={() => router.push("/requests")}
+          className="bg-red-500 w-[65%] hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Back
+        </Button>
       </div>
       <ToastContainer />
     </>
   );
 };
 
-export default Requirement;
+export default Pitch;
