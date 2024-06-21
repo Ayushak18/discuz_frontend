6ce9b82b8f1c916a3279f1763053032d72ecb009
Ayush Kamboj <aayushkamboj7@gmail.com>
2024-06-22 05:07:57 +0530

    Code to filter Accepted pitch for vendor added on its dashboard

--- src/app/(main)/vendor_dashboard/page.tsx
+++ src/app/(main)/vendor_dashboard/page.tsx
@@ -13,6 +13,9 @@ import { CardWithForm } from "../page-components/Card";
 import SideNav from "../page-components/Side-Nav";
 import "../../../../public/style/scroll-bar.css";
 import Cookies from "js-cookie";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import ExpandedVendorTile from "../page-components/ExpandedPageEdit";
 
 const Dashboard = () => {
   const getUserDataFromCookie = () => {
@@ -20,22 +23,29 @@ const Dashboard = () => {
     return userData ? JSON.parse(userData) : null;
   };
 
+  const [showTile, setShowTile] = useState(false);
+  const [reqData, setReqData] = useState(undefined);
+
+  const pathname = usePathname();
+
   let userData: any = getUserDataFromCookie();
 
-  console.log(userData?.user?.sector);
+  // console.log(userData?.user?.email);
 
   const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ["requirements_for_vendor"],
+    queryKey: ["accepted_pitches_for_vendor"],
     queryFn: async () => {
       const response = await axios.post(
-        `https://discuz-backend.onrender.com/requirements/sector`,
+        `https://discuz-backend.onrender.com/virtual_pitch/email`,
         {
-          req_sector: "Automobile",
+          company_email: userData?.user?.email,
         }
       );
       return response.data;
     },
   });
+
+  // console.log(data?.data[0]);
 
   if (isPending) {
     return (
@@ -50,41 +60,80 @@ const Dashboard = () => {
     );
   } else {
     return (
-      <div className="flex justify-around">
-        <SideNav />
-        <div className="w-[78%] h-[98vh] my-[1vh]">
-          <div className="rounded-3xl pb-4 bg-[#fff] ">
-            <div className="flex items-center w-[97%] justify-between mx-auto pt-4 mb-2">
-              <h2 className="text-[#3E435D]  text-[28px] font-semibold">
-                Hello Dency!
-              </h2>
-              <div className="flex w-[25%]">
-                <Input
-                  className="rounded-xl border-gray-300 placeholder:text-gray-400 border-2"
-                  type="text"
-                  placeholder="Search..."
-                />
-              </div>
+      <>
+        {showTile ? (
+          <>
+            <div className="flex justify-around">
+              <SideNav />
+              <ExpandedVendorTile reqData={reqData} setShowTile={setShowTile} />
             </div>
-            <Separator className="bg-[#d7d7d7] w-[97%] mx-auto my-2  h-[2px]" />
+          </>
+        ) : (
+          <>
+            <div className="flex justify-around">
+              <SideNav />
+              <div className="w-[78%] h-[98vh] my-[1vh]">
+                <div className="rounded-3xl pb-4 bg-[#fff] ">
+                  <div className="flex items-center w-[97%] justify-between mx-auto pt-4 mb-2">
+                    <h2 className="text-[#3E435D]  text-[28px] font-semibold">
+                      Hello Dency!
+                    </h2>
+                    <div className="flex w-[25%]">
+                      <Input
+                        className="rounded-xl border-gray-300 placeholder:text-gray-400 border-2"
+                        type="text"
+                        placeholder="Search..."
+                      />
+                    </div>
+                  </div>
+                  <Separator className="bg-[#d7d7d7] w-[97%] mx-auto my-2  h-[2px]" />
 
-            <div className="flex flex-row overflow-y-scroll no-scrollbar h-[87vh] mx-auto w-[97%]">
-              <div className="flex flex-row  justify-between flex-wrap">
-                {data?.data?.map((content: any) => {
-                  return (
-                    <CardWithForm
-                      image={content.imgPath || "/images/image.png"}
-                      isRequested={false}
-                      title={content.product}
-                      description={content.product_desc}
-                    />
-                  );
-                })}
+                  <div className="flex flex-row overflow-y-scroll no-scrollbar h-[87vh] mx-auto w-[97%]">
+                    <div className="flex flex-row  justify-between flex-wrap">
+                      {data?.data?.map((content: any) => {
+                        if (
+                          userData?.user?.role === "Vendor" &&
+                          pathname.includes("vendor_dashboard") &&
+                          content.isAccepted
+                        ) {
+                          // console.log(content.isAccepted);
+                          return (
+                            <CardWithForm
+                              image={content.imgPath || "/images/image.png"}
+                              isRequested={false}
+                              title={content.product || content.pitch_title}
+                              description={
+                                content.product_desc || content.product_details
+                              }
+                              setShowTile={setShowTile}
+                              data={content}
+                              setReqData={setReqData}
+                            />
+                          );
+                        } else if (!pathname.includes("vendor_dashboard")) {
+                          return (
+                            <CardWithForm
+                              image={content.imgPath || "/images/image.png"}
+                              isRequested={false}
+                              title={content.product || content.pitch_title}
+                              description={
+                                content.product_desc || content.product_details
+                              }
+                              setShowTile={setShowTile}
+                              data={content}
+                              setReqData={setReqData}
+                            />
+                          );
+                        }
+                      })}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+          </>
+        )}
+      </>
     );
   }
 };
